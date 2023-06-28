7090cd28700d99bbed1bc7a1918f724781e08d6d
Ramkh05ho <rkhosho@cps.edu>
2023-06-28 09:36:31 -0500

    RamiLab5

---
+++ src/RamiLab5.ino
@@ -0,0 +1,22 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+int cMax = 2500;
+int cMin = 0;
+
+void setup() {
+
+  pinMode(D6, OUTPUT);
+  pinMode(A5, INPUT);
+
+}
+
+void loop() {
+
+  int reading = analogRead(A5);
+  int Maxx = max(cMax, reading);
+  int Minn = min(cMin, reading);
+  int brightness = map(reading, Minn, Maxx, 0, 150);
+  analogWrite(D6, brightness);
+
+}
